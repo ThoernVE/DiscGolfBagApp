11d30d25649c4a5b48dd4f2e17d54f2b8a42fad4
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2024-10-08 15:31:11 +0200

    Fixed loading-issue. Separated dictionary into two separate Lists with usernames and passwords. Fixed log-in methods and cleaned up log-in UI. Also added comments for ease of understanding the code. Refactoring and cleaning up.

--- DiscBag/DiscBag/LogIn.cs
+++ DiscBag/DiscBag/LogIn.cs
@@ -11,13 +11,23 @@ namespace DiscBag
 
         public static string filePath = Path.Combine(path, "saveFile.json");
 
-        internal static void Start()
+        internal static void Start() //starting menu, loads data, prints available users and initiates startmenu
+        {
+            LoadLoginData(); //loads data from local files
+            //Console.WriteLine("Available users:");
+           // UserClass.PrintUser(); //prints available users.
+            StartMenu();
+        }
+
+        public static void StartMenu() //menu for navigating login
         {
             Console.WriteLine("What would you like to do today\n" +
                 "1. Login\n" +
                 "2. Create a new user\n" +
-                "Available users:");
-            UserClass.PrintUser();
+                "Press 'Escape' to close application\n");
+            Console.WriteLine("Available users:");
+            UserClass.PrintUser(); //prints available users.
+
 
             switch (Console.ReadKey().Key)
             {
@@ -27,46 +37,104 @@ namespace DiscBag
 
                 case ConsoleKey.D2:
                     UserClass.AddUser();
-                    Start();
                     SaveLogInData();
                     DiscGolfBag.Cleanup();
+                    StartMenu();
+                    break;
+
+                case ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter a valid option");
+                    DiscGolfBag.Cleanup();
+                    StartMenu();
                     break;
             }
-
         }
 
-        private static void LogInMethod()
+        private static void LogInMethod() //method that takes two userinputs and checks them against the list to make sure both password and usernames are the same as input. Not functioning, needs rework?
         {
             Console.WriteLine("\nPlease enter your username");
             string attemptedUsername = Console.ReadLine();
             Console.WriteLine("Please enter your password");
             string attemptedPassword = Console.ReadLine();
-            UserClass.CheckUser(attemptedUsername, attemptedPassword);
+            bool existingUser = UserClass.userNames.Contains(attemptedUsername); //checks if the username exists
+            if (existingUser == true)
+            {
+                for (int i = 0; i < UserClass.userNames.Count; i++) //checks every iteration of username and password to make sure both sides match
+                {
+                    if (attemptedUsername == UserClass.userNames[i] && attemptedPassword == UserClass.passwords[i])
+                    {
+                        Console.WriteLine("Welcome to the Discbag Application!");
+                        LogIn.filePath = Path.Combine(LogIn.path, $"{attemptedUsername}saveFile.json"); //sets filepath to the users specific file
+                        break;
+
+                    }
+                    else if (attemptedUsername == UserClass.userNames[i] && attemptedPassword != UserClass.passwords[i])
+                    {
+                        Console.WriteLine("Password was wrong. Please try again."); 
+                        DiscGolfBag.Cleanup();
+                        StartMenu();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("User was not found. Please try again");
+                DiscGolfBag.Cleanup();
+                StartMenu();
+            }
             DiscGolfBag.LoadData();
+            
         }
+
 
         public static void SaveLogInData()
         {
+            SaveUserNames();
+            SavePasswords();
+        }
 
-            var serializedObject = Newtonsoft.Json.JsonConvert.SerializeObject(UserClass.users);
-
+        public static void SaveUserNames()
+        {
+            var serializedObject = Newtonsoft.Json.JsonConvert.SerializeObject(UserClass.userNames);
             string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string filePath = Path.Combine(path, "saveFileUsers.json");
+
             using (StreamWriter sw = new StreamWriter(filePath))
             {
                 sw.Write(serializedObject);
-
             }
         }
+
+        public static void SavePasswords()
+        {
+            var serializedObject = Newtonsoft.Json.JsonConvert.SerializeObject(UserClass.passwords);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string filePath = Path.Combine(path, "saveFilePasswords.json");
+
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                sw.Write(serializedObject);
+            }
+        }   
 
 
         public static void LoadLoginData()
         {
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string filePath = Path.Combine(path, "saveFileUsers.json");
+            LoadUserNames();
+            LoadPasswords(); 
+        }
 
+        public static void LoadUserNames()
+        {
             try
             {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string filePath = Path.Combine(path, "saveFileUsers.json");
+
                 string content = null;
                 using (StreamReader sr = new StreamReader(filePath))
                 {
@@ -74,18 +142,42 @@ namespace DiscBag
                 }
 
 
-                Dictionary<string, string> loadedUsers = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
+                List<string> loadedUsers = JsonConvert.DeserializeObject<List<string>>(content);
 
-                foreach (var user in loadedUsers)
+                foreach (string user in loadedUsers)
                 {
-                    UserClass.users.Add(user.Key, user.Value);
+                    UserClass.userNames.Add(user);
                 }
             }
-            catch 
+            catch
             {
             }
+        }
+
+        public static void LoadPasswords()
+        {
+            try
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string filePath = Path.Combine(path, "saveFilePasswords.json");
+
+                string content = null;
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    content = sr.ReadToEnd();
+                }
 
 
+                List<string> loadedPasswords = JsonConvert.DeserializeObject<List<string>>(content);
+
+                foreach (string password in loadedPasswords)
+                {
+                    UserClass.passwords.Add(password);
+                }
+            }
+            catch
+            {
+            }
         }
 
     }
--- DiscBag/DiscBag/UserClass.cs
+++ DiscBag/DiscBag/UserClass.cs
@@ -5,58 +5,44 @@ using System.Runtime.CompilerServices;
 
 namespace DiscBag
 {
-    internal class UserClass
+    internal class UserClass //class for user that cointains UserID and password
     {
-        public static string UserId;
-        public static string Password;
+        public string UserId;
+        public string Password;
 
-        public UserClass(string userId, string password)
+        public UserClass(string userId, string password) //constructor to create objects as userclass.
         {
             UserId = userId;
             Password = password;
         }
 
-        static public Dictionary<string, string> users = new Dictionary<string, string>();
-        
-        public static void AddUser()
+        static public List<string> userNames = new List<string>(); //defining a dictionary for users.
+        static public List<string> passwords = new List<string>();
+
+        public static void AddUser() //method for adding user, takes userinputs, creates an object as a user and puts it into the dictionary
         {
             Console.WriteLine("\nPlease enter username");
             string userName = Console.ReadLine();
             Console.WriteLine("Please enter password");
             string password = Console.ReadLine();
             UserClass user = new UserClass(userName, password);
-            users.Add(userName, password);
+            userNames.Add(user.UserId);
+            passwords.Add(user.Password);
         }
 
-        public static void PrintUser()
+        public static void PrintUser() //method for printing users.
         {
-            foreach (var user in users) 
-                Console.WriteLine(user.Key);
-        }
-
-       /* public  override string ToString()
-        {
-            return $"{UserId}";
-        }*/
-
-
-        internal static void CheckUser(string userName, string password)
-        {
-            foreach (var user in users)
+            foreach (string user in userNames)
             {
-                if (userName == UserId && password == Password)
-                {
-                    Console.WriteLine("Welcome to the Discbag Application!");
-                    LogIn.filePath =  Path.Combine(LogIn.path, $"{userName}saveFile.json");
-                    
-                }
-                else
-                {
-                    Console.WriteLine("Login-attempt failed");
-                    LogIn.Start();
-                }
+                Console.WriteLine(user);
             }
-            
+                
         }
+
+        public override string ToString() //To-string override for printing
+        {
+            return $"{userNames}";
+        }
+
     }
 }
